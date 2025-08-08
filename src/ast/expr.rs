e041aebae70143081d9104b037f1a40cba17bdb4
Wan Zhaoyuan <multicolor@sjtu.edu.cn>
2025-08-08 23:01:59 +0800

    通过 Pratt Parser 算法解决了运算符优先级问题

---
+++ src/ast/expr.rs
@@ -0,0 +1,376 @@
+use crate::{
+    ast::Visitable,
+    lexer::{Token, TokenIter},
+    tokens::TokenType,
+    utils::string::{parse_number_literal, parse_quoted_content},
+};
+
+#[derive(Debug)]
+pub struct Expr {
+    pub kind: ExprKind,
+}
+
+#[derive(Debug)]
+pub enum ExprKind {
+    // Array(ThinVec<P<Expr>>),
+    // ConstBlock(AnonConst),
+    // Call(P<Expr>, ThinVec<P<Expr>>),
+    // MethodCall(Box<MethodCall>),
+    // Tup(ThinVec<P<Expr>>),
+    Binary(Binary),
+    Unary(Unary),
+    Lit(Lit),
+    // Cast(P<Expr>, P<Ty>),
+    // Let(P<Pat>, P<Expr>, Span, Recovered),
+    // If(P<Expr>, P<Block>, Option<P<Expr>>),
+    // While(P<Expr>, P<Block>, Option<Label>),
+    // ForLoop {
+    //     pat: P<Pat>,
+    //     iter: P<Expr>,
+    //     body: P<Block>,
+    //     label: Option<Label>,
+    //     kind: ForLoopKind,
+    // },
+    // Loop(P<Block>, Option<Label>, Span),
+    // Match(P<Expr>, ThinVec<Arm>, MatchKind),
+    // Block(P<Block>, Option<Label>),
+    // Assign(P<Expr>, P<Expr>, Span),
+    // AssignOp(AssignOp, P<Expr>, P<Expr>),
+    // Field(P<Expr>, Ident),
+    // Index(P<Expr>, P<Expr>, Span),
+    // Range(Option<P<Expr>>, Option<P<Expr>>, RangeLimits),
+    // Underscore,
+    // Path(Option<P<QSelf>>, Path),
+    // Break(Option<Label>, Option<P<Expr>>),
+    // Continue(Option<Label>),
+    // Ret(Option<P<Expr>>),
+    // Struct(P<StructExpr>),
+    // Repeat(P<Expr>, AnonConst),
+    // Paren(P<Expr>),
+    // Become(P<Expr>),
+}
+
+impl Visitable for Expr {
+    fn eat(iter: &mut TokenIter) -> Option<Self> {
+        Self::eat_with_priority(iter, 0)
+    }
+}
+
+impl Expr {
+    pub fn eat_with_priority(iter: &mut TokenIter, min_priority: usize) -> Option<Self> {
+        let mut kind: Option<ExprKind> = None;
+        kind = kind.or_else(|| Unary::eat(iter).map(ExprKind::Unary));
+        kind = kind.or_else(|| Lit::eat(iter).map(ExprKind::Lit));
+
+        kind = kind.map(|expr1| {
+            if let Some(helper) = BinaryHelper::eat_with_priority(iter, min_priority) {
+                ExprKind::Binary(Binary(helper.0, Box::new(Expr { kind: expr1 }), helper.1))
+            } else {
+                expr1
+            }
+        });
+
+        Some(Expr { kind: kind? })
+    }
+}
+
+#[derive(Debug)]
+pub struct Lit {
+    pub kind: LitKind,
+    pub symbol: String,
+    pub suffix: Option<String>,
+}
+
+#[derive(Debug)]
+pub enum LitKind {
+    Bool,
+    Byte,
+    Char,
+    Integer,
+    Float,
+    Str,
+    StrRaw(u8),
+    ByteStr,
+    ByteStrRaw(u8),
+    CStr,
+    CStrRaw(u8),
+}
+
+impl Visitable for Lit {
+    fn eat(iter: &mut TokenIter) -> Option<Lit> {
+        let mut using_iter = iter.clone();
+
+        let ret = using_iter.next()?.try_into().ok()?;
+
+        iter.update(using_iter);
+        Some(ret)
+    }
+}
+
+impl<'a> TryInto<Lit> for &Token<'a> {
+    type Error = ();
+
+    fn try_into(self) -> Result<Lit, Self::Error> {
+        match self.token_type {
+            TokenType::True | TokenType::False => Ok(Lit {
+                kind: LitKind::Bool,
+                symbol: self.lexeme.to_owned(),
+                suffix: None,
+            }),
+
+            TokenType::Byte => {
+                let (symbol, suffix) = parse_quoted_content(self.lexeme, '\'').ok_or(())?;
+                Ok(Lit {
+                    kind: LitKind::Byte,
+                    symbol,
+                    suffix,
+                })
+            }
+
+            TokenType::Character => {
+                let (symbol, suffix) = parse_quoted_content(self.lexeme, '\'').ok_or(())?;
+                Ok(Lit {
+                    kind: LitKind::Char,
+                    symbol,
+                    suffix,
+                })
+            }
+
+            TokenType::Integer => {
+                let (symbol, suffix) = parse_number_literal(self.lexeme);
+                Ok(Lit {
+                    kind: LitKind::Integer,
+                    symbol,
+                    suffix,
+                })
+            }
+
+            TokenType::Float => {
+                let suffix_pos = self
+                    .lexeme
+                    .find(|c: char| !(c.is_ascii_digit() || c == '.' || c == '_'));
+
+                let symbol =
+                    suffix_pos.map_or(self.lexeme.to_owned(), |pos| self.lexeme[..pos].to_owned());
+                let suffix = suffix_pos.map(|pos| self.lexeme[pos..].to_owned());
+
+                Ok(Lit {
+                    kind: LitKind::Float,
+                    symbol,
+                    suffix,
+                })
+            }
+
+            TokenType::String => {
+                let (symbol, suffix) = parse_quoted_content(self.lexeme, '\"').ok_or(())?;
+                Ok(Lit {
+                    kind: LitKind::Str,
+                    symbol,
+                    suffix,
+                })
+            }
+
+            TokenType::RawString => {
+                let (sharps, other) = self.lexeme.split_once('\"').ok_or(())?;
+                let (symbol, suffix_with_sharps) = other.rsplit_once('\"').ok_or(())?;
+                let sharp_num = sharps.len() - 1;
+                let suffix = &suffix_with_sharps[sharp_num..];
+                let suffix = if suffix.is_empty() {
+                    None
+                } else {
+                    Some(suffix.to_owned())
+                };
+
+                Ok(Lit {
+                    kind: LitKind::StrRaw(sharp_num as u8),
+                    symbol: symbol.to_owned(),
+                    suffix,
+                })
+            }
+
+            TokenType::ByteString => {
+                let content = &self.lexeme[1..];
+                let (symbol, suffix) = parse_quoted_content(content, '\"').ok_or(())?;
+                Ok(Lit {
+                    kind: LitKind::ByteStr,
+                    symbol,
+                    suffix,
+                })
+            }
+
+            TokenType::RawByteString => {
+                let without_b = &self.lexeme[1..];
+                let (sharps, other) = without_b.split_once('\"').ok_or(())?;
+                let (symbol, suffix_with_sharps) = other.rsplit_once('\"').ok_or(())?;
+                let sharp_num = sharps.len() - 1;
+                let suffix = &suffix_with_sharps[sharp_num..];
+                let suffix = if suffix.is_empty() {
+                    None
+                } else {
+                    Some(suffix.to_owned())
+                };
+
+                Ok(Lit {
+                    kind: LitKind::ByteStrRaw(sharp_num as u8),
+                    symbol: symbol.to_owned(),
+                    suffix,
+                })
+            }
+
+            TokenType::CString => {
+                let content = &self.lexeme[1..];
+                let (symbol, suffix) = parse_quoted_content(content, '\"').ok_or(())?;
+                Ok(Lit {
+                    kind: LitKind::CStr,
+                    symbol,
+                    suffix,
+                })
+            }
+
+            TokenType::RawCString => {
+                let without_c = &self.lexeme[1..];
+                let (sharps, other) = without_c.split_once('\"').ok_or(())?;
+                let (symbol, suffix_with_sharps) = other.rsplit_once('\"').ok_or(())?;
+                let sharp_num = sharps.len() - 1;
+                let suffix = &suffix_with_sharps[sharp_num..];
+                let suffix = if suffix.is_empty() {
+                    None
+                } else {
+                    Some(suffix.to_owned())
+                };
+
+                Ok(Lit {
+                    kind: LitKind::CStrRaw(sharp_num as u8),
+                    symbol: symbol.to_owned(),
+                    suffix,
+                })
+            }
+
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Unary(pub UnOp, pub Box<Expr>);
+
+impl Visitable for Unary {
+    fn eat(iter: &mut TokenIter) -> Option<Self> {
+        let mut using_iter = iter.clone();
+
+        let unop = using_iter.next()?.try_into().ok()?;
+        let expr = Expr::eat_with_priority(&mut using_iter, 1000000)?;
+
+        iter.update(using_iter);
+        Some(Self(unop, Box::new(expr)))
+    }
+}
+
+#[derive(Debug)]
+pub enum UnOp {
+    Deref,
+    Not,
+    Neg,
+}
+
+impl<'a> TryInto<UnOp> for &Token<'a> {
+    type Error = ();
+
+    fn try_into(self) -> Result<UnOp, Self::Error> {
+        match self.token_type {
+            TokenType::Star => Ok(UnOp::Deref),
+            TokenType::Not => Ok(UnOp::Not),
+            TokenType::Minus => Ok(UnOp::Neg),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Binary(pub BinOp, pub Box<Expr>, pub Box<Expr>);
+
+#[derive(Debug)]
+pub struct BinaryHelper(pub BinOp, pub Box<Expr>);
+
+impl BinaryHelper {
+    fn eat_with_priority(iter: &mut TokenIter, min_priority: usize) -> Option<Self> {
+        let mut using_iter = iter.clone();
+
+        let op: BinOp = using_iter.next()?.try_into().ok()?;
+
+        if op.get_priority() < min_priority {
+            return None;
+        }
+
+        let expr2 = Expr::eat_with_priority(&mut using_iter, op.get_priority() + 1)?;
+
+        iter.update(using_iter);
+        Some(BinaryHelper(op, Box::new(expr2)))
+    }
+}
+
+#[derive(Debug)]
+pub enum BinOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Rem,
+    And,
+    Or,
+    BitXor,
+    BitAnd,
+    BitOr,
+    Shl,
+    Shr,
+    Eq,
+    Lt,
+    Le,
+    Ne,
+    Ge,
+    Gt,
+}
+
+impl BinOp {
+    pub fn get_priority(&self) -> usize {
+        match self {
+            Self::Or => 10,
+            Self::And => 20,
+            Self::Eq | Self::Ne | Self::Lt | Self::Gt | Self::Le | Self::Ge => 30,
+            Self::BitOr => 40,
+            Self::BitXor => 50,
+            Self::BitAnd => 60,
+            Self::Shl | Self::Shr => 70,
+            Self::Add | Self::Sub => 80,
+            Self::Mul | Self::Div | Self::Rem => 90,
+        }
+    }
+}
+
+impl<'a> TryInto<BinOp> for &Token<'a> {
+    type Error = ();
+
+    fn try_into(self) -> Result<BinOp, Self::Error> {
+        match self.token_type {
+            TokenType::Plus => Ok(BinOp::Add),
+            TokenType::Minus => Ok(BinOp::Sub),
+            TokenType::Star => Ok(BinOp::Mul),
+            TokenType::Slash => Ok(BinOp::Div),
+            TokenType::Percent => Ok(BinOp::Rem),
+            TokenType::AndAnd => Ok(BinOp::And),
+            TokenType::OrOr => Ok(BinOp::Or),
+            TokenType::Caret => Ok(BinOp::BitXor),
+            TokenType::And => Ok(BinOp::BitAnd),
+            TokenType::Or => Ok(BinOp::BitOr),
+            TokenType::Shl => Ok(BinOp::Shl),
+            TokenType::Shr => Ok(BinOp::Shr),
+            TokenType::EqEq => Ok(BinOp::Eq),
+            TokenType::Lt => Ok(BinOp::Lt),
+            TokenType::Le => Ok(BinOp::Le),
+            TokenType::Ne => Ok(BinOp::Ne),
+            TokenType::Ge => Ok(BinOp::Ge),
+            TokenType::Gt => Ok(BinOp::Gt),
+            _ => Err(()),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use c0mpiler::{
-    ast::{Expr, Visitable},
+    ast::{Visitable, expr::Expr},
     lexer::{Lexer, TokenBuffer},
 };
 
 fn main() {
-    let test_str = r###" 1 * 1 + 1 "###;
+    let test_str = r###" !1 + -1 * 1 "###;
     let lexer = Lexer::new(test_str);
     let buffer = TokenBuffer::new(lexer);
 
     let mut iter = buffer.iter();
     let expr = Expr::eat(&mut iter);
-
-    println!("{expr:#?}");
+    if iter.next().is_some() {
+        println!("Invalid!");
+    } else {
+        println!("{expr:#?}");
+    }
 }
